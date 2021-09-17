0e2691dbe0addb3c8d5216232dc529d20a6a7a43
Ana Novak <anastasy.d@gmail.com>
2021-09-17 14:16:13 +0200

    Use hash to improve runtime complexity to 2n instead of n*n

--- two-sum.js
+++ two-sum.js
@@ -4,10 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  for (let [index, value] of nums.entries()) {
-    const secondNumberIndex = nums.indexOf(target - value, index + 1);
-    if (secondNumberIndex !== -1) {
-      return [index, secondNumberIndex];
+  let result = [];
+  const hash = {};
+
+  nums.forEach((value, index) => {
+    hash[target - value] = index;
+  });
+
+  nums.forEach((value, index) => {
+    if (hash[value] && hash[value] !== index) {
+      result = [index, hash[value]];
     }
-  }
+  });
+  return result;
 };
