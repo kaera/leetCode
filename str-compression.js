afc2f6e6abdb26c7d704ab3ef0069ffb54845dd7
Ana Novak <anastasy.d@gmail.com>
2021-12-30 23:17:25 +0100

    Solution for string compression with no use of extra memory

--- str-compression.js
+++ str-compression.js
@@ -3,20 +3,10 @@
  * @return {string}
  */
 var strCompression = function (str) {
-  let result = "";
-  let count = 1;
-
-  for (let i = 0; i <= str.length - 1; i++) {
-    let currentLetter = str[i];
-
-    if (currentLetter === str[i + 1]) {
-      count += 1;
-    } else {
-      result += `${currentLetter}${count}`;
-      currentLetter = str[i + 1];
-      count = 1;
+  for (let i = str.length; i--; i >= 0) {
+    if (str[i - 1] === str[i]) {
+      str = str.substr(0, i - 1) + str.substr(i);
     }
   }
-
-  return result;
+  return str;
 };
